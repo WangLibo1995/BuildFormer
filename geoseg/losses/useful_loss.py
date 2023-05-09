68259584c1d228f92109c624d53c329fd35e5da8
Libo Wang <574961738@qq.com>
2023-05-09 12:27:34 +0800

    Update useful_loss.py

--- geoseg/losses/useful_loss.py
+++ geoseg/losses/useful_loss.py
@@ -43,8 +43,79 @@ class EdgeLoss(nn.Module):
         return edge_loss
 
     def forward(self, logits, targets):
-        loss = self.main_loss(logits, targets) + self.compute_edge_loss(logits, targets) * self.edge_factor
+        loss = (self.main_loss(logits, targets) + self.compute_edge_loss(logits, targets) * self.edge_factor) / (self.edge_factor + 1.0)
         return loss
+
+
+class OHEM_CELoss(nn.Module):
+
+    def __init__(self, thresh=0.7, ignore_index=255):
+        super(OHEM_CELoss, self).__init__()
+        self.thresh = thresh
+        self.ignore_index = ignore_index
+        self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_index, reduction='none')
+
+    def forward(self, logits, labels):
+        thresh = -torch.log(torch.tensor(self.thresh, requires_grad=False, dtype=torch.float)).cuda(device=logits.device)
+        n_min = labels[labels != self.ignore_index].numel() // 16
+        loss = self.criteria(logits, labels).view(-1)
+        loss_hard = loss[loss > thresh]
+        if loss_hard.numel() < n_min:
+            loss_hard, _ = loss.topk(n_min)
+        return torch.mean(loss_hard)
+
+
+class ProbOhemCrossEntropy2d(nn.Module):
+    def __init__(self, ignore_label=255, reduction='mean', thresh=0.7, min_kept=256,
+                 down_ratio=1, use_weight=False):
+        super(ProbOhemCrossEntropy2d, self).__init__()
+        self.ignore_label = ignore_label
+        self.thresh = float(thresh)
+        self.min_kept = int(min_kept)
+        self.down_ratio = down_ratio
+        if use_weight:
+            weight = torch.FloatTensor(
+                [0.8373, 0.918, 0.866, 1.0345, 1.0166, 0.9969, 0.9754, 1.0489,
+                 0.8786, 1.0023, 0.9539, 0.9843, 1.1116, 0.9037, 1.0865, 1.0955,
+                 1.0865, 1.1529, 1.0507])
+            self.criterion = torch.nn.CrossEntropyLoss(reduction=reduction,
+                                                       weight=weight,
+                                                       ignore_index=ignore_label)
+        else:
+            self.criterion = torch.nn.CrossEntropyLoss(reduction=reduction,
+                                                       ignore_index=ignore_label)
+
+    def forward(self, pred, target):
+        b, c, h, w = pred.size()
+        target = target.view(-1)
+        valid_mask = target.ne(self.ignore_label)
+        target = target * valid_mask.long()
+        num_valid = valid_mask.sum()
+
+        prob = F.softmax(pred, dim=1)
+        prob = (prob.transpose(0, 1)).reshape(c, -1)
+
+        if self.min_kept > num_valid:
+            print('Labels: {}'.format(num_valid))
+        elif num_valid > 0:
+            prob = prob.masked_fill_(~valid_mask, 1)
+            mask_prob = prob[
+                target, torch.arange(len(target), dtype=torch.long)]
+            threshold = self.thresh
+            if self.min_kept > 0:
+                index = mask_prob.argsort()
+                threshold_index = index[min(len(index), self.min_kept) - 1]
+                if mask_prob[threshold_index] > self.thresh:
+                    threshold = mask_prob[threshold_index]
+                kept_mask = mask_prob.le(threshold)     # 概率小于阈值的挖出来
+                target = target * kept_mask.long()
+                valid_mask = valid_mask * kept_mask
+                # logger.info('Valid Mask: {}'.format(valid_mask.sum()))
+
+        target = target.masked_fill_(~valid_mask, self.ignore_label)
+        target = target.view(b, h, w)
+
+        return self.criterion(pred, target)
 
 
 if __name__ == '__main__':
